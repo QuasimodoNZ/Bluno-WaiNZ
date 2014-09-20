7be1112bdb0255cc90c5b5f9dd251cce16093216
Hannah Glaeser <hannah.glaeser@gmail.com>
2014-09-20 15:52:58 +1200

    (mostly) complete river data class

--- BlunoBasicDemo/src/com/example/blunobasicdemo/RiverData.java
+++ BlunoBasicDemo/src/com/example/blunobasicdemo/RiverData.java
@@ -1,27 +1,102 @@
 package com.example.blunobasicdemo;
 
-import org.json.JSONObject;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
+import org.json.*;
+import android.content.Context;
+import android.location.Location;
+import java.text.ParseException;
 
-import android.text.format.Time;
 
 public class RiverData {
 	
 	float lat,lon;
-	Time readingDate;
-	float tempreture;
+	Date readingDate;
+	float temperature;
 	float conductivity;
+	String ID, session;
+	Context context;
+	
+	public RiverData(Context context){
+		this.context = context;
+	}
 
-	public RiverData(){
-		
+	public void RiverDataErr(){
+		//TODO error state - empty one
 	}
 	
-	public RiverData(JSONObject j){
+	public void RiverDataJSON(JSONObject j){
+		String date_i = null;
+		String conductivity_i = null;
+		String temperature_i = null;
 		
+		SimpleDateFormat formatter = new SimpleDateFormat("yyyy.MM.dd 'at' HH:mm:ss a");
+		try{
+			//Get strings from JSON
+			ID = j.getString("id");
+			session = j.getString("session");
+			date_i = j.getString("time");
+		    conductivity_i = j.getString("ec");
+			temperature_i = j.getString("temp");
+		}
+		catch (JSONException e){
+			//TODO log error
+			e.printStackTrace();
+		}
+		try{
+			//Convert the ones that aren't strings
+			readingDate = (Date)formatter.parse(date_i);
+			conductivity = Float.parseFloat(conductivity_i);
+			temperature = Float.parseFloat(temperature_i);
+		}
+		catch (ParseException e){
+			//TODO log error
+			e.printStackTrace();
+		}
+		catch (NumberFormatException e){
+			//TODO log error
+			e.printStackTrace();
+		}
 	}
 	
-	public RiverData(JSONObject j, UserLocationTracker t){
-		
-		
+	public void RiverDataComplete(JSONObject j, UserLocationTracker t){
+		this.RiverDataJSON(j);
+		lat = t.getLat();
+		lon = t.getLon();
+	}
+	
+	public void setupGPS(){
+		//TODO make sure this shit works
+		UserLocationTracker t = new UserLocationTracker(context);
+		Location l = t.getLocation();
+	}
+
+	public float getTemperature() {
+		return temperature;
+	}
+	
+	public float getConductivity() {
+		return conductivity;
+	}
+	
+	public float getLat() {
+		return lat;
+	}
+	
+	public float getLon() {
+		return lon;
+	}
+	
+	public Date getReadingDate() {
+		return readingDate;
+	}
+	
+	public String getID() {
+		return ID;
+	}
+	
+	public String getSession() {
+		return session;
 	}
 	
 }
