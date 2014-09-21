49a4c3c8e6892bc6d3a810852a27baac5f875d05
Peter Bennie <peter.bennie@paradise.net.nz>
2014-09-22 01:02:21 +1200

    added circular progress bars.

--- BlunoBasicDemo/src/com/example/blunobasicdemo/Results.java
+++ BlunoBasicDemo/src/com/example/blunobasicdemo/Results.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ProgressBar;
@@ -15,11 +16,16 @@ public class Results extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_results);
 		
-		ProgressBar pb = (ProgressBar) this.findViewById(R.id.progressBarToday);
+		ProgressBar condPb = (ProgressBar) this.findViewById(R.id.conductivity_progress);
+		ProgressBar tempPb = (ProgressBar) this.findViewById(R.id.temperature_progress);
 	
 		Animation an = new RotateAnimation(0.0f, 90.0f, 250f, 273f);
 		an.setFillAfter(true);
-		pb.startAnimation(an);
+		condPb.startAnimation(an);
+		tempPb.startAnimation(an);
+		
+	    //Remove title bar
+	    this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 		
 	}
 
